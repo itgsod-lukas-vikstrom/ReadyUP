0f3023efeb1ad993b07f4c15f97b477079516ee3
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-02-05 11:48:37 +0100

    changed models

--- models/roomuser.rb
+++ models/roomuser.rb
@@ -1,7 +1,10 @@
-class Room_Users
+class RoomUser
   include DataMapper::Resource
+
   property :id, Serial
   property :ready_until, DateTime, :required => true
   property :leader, Boolean, :required => true
 
+  belongs_to :room
+  belongs_to :user
 end
