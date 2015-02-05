1c41bbd55414bda31c2e6496958479b4adce4e0c
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-02-05 11:48:32 +0100

    changed seeding

--- db/seed.rb
+++ db/seed.rb
@@ -2,18 +2,18 @@ class Seeder
   def self.seed
     self.room
     self.user
-    #self.room_users
+    self.room_users
   end
 
   def self.room
-    Room.create(url: 'ddhwbz', name: 'dogecoin', size: 5, public: 'public', game: 'Dota2', language: 'Swedish',   )
+    Room.create(url: 'ddhwbz', name: 'dogecoin', size: 5, public: 'public', game: 'Dota2', language: 'Swedish')
   end
   def self.user
-    User.create(name: 'GaemMastar', admin: true, login_provider: 'Stem', login_key: '%hjh34hhg2_jkh5j2' )
+    User.create(name: 'GaemMastar', admin: true, login_provider: 'Steam', login_key: '%hjh34hhg2_jkh5j2' )
   end
-  #def self.room_users
-  #  RoomUser.create(room_id: 1, user_id: 1)
-  #end
+  def self.room_users
+    RoomUser.create(room_id: 1, user_id: 1, ready_until: "12:45", leader: false)
+  end
 end
 
 
