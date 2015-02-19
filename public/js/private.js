66c6bdc2d17a4353b9e9bcd85d7a62d44c6e64f3
itgsod-lukas-vikstrom <lukas.vikstrom@elev.it-gymnasiet.se>
2015-02-19 11:38:38 +0100

    Added a submit button in the Report form. Closes #34

--- public/js/private.js
+++ public/js/private.js
@@ -21,10 +21,12 @@ function hidereport() {
     if(visible){
         document.getElementById("reportname").style.visibility="hidden";
         document.getElementById("reportdescription").style.visibility="hidden";
+        document.getElementById("submit").style.visibility="hidden";
         visible = false
     } else {
         document.getElementById("reportname").style.visibility="visible";
         document.getElementById("reportdescription").style.visibility="visible";
+        document.getElementById("submit").style.visibility="visible";
         visible = true
     }
 console.log(visible)
