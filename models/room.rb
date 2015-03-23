4f70d838c366b3b22c890e31b97981f45861bcc8
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-03-23 14:09:02 +0100

    added users method to room model Closes #67

--- models/room.rb
+++ models/room.rb
@@ -12,4 +12,11 @@ class Room
 
   has n, :user, :through => Resource
 
+  def users
+    users = []
+    for roomuser in RoomUser.all(room: self)
+      users << User.first(id: roomuser.user_id)
+    end
+    return users
+  end
 end
