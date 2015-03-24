daba71456096f70cd77a9681b5432fa9d3d61c09
itgsod-ludwig-liljabacklund <ludwig.liljabacklun@LLB-Macbook.local>
2015-03-24 11:03:38 +0100

    added creator id's to seed file, added admin created rooms for testing Closes #45

--- db/seed.rb
+++ db/seed.rb
@@ -8,13 +8,15 @@ class Seeder
   end
 
   def self.room
-    Room.create(url: "417kd4cyfs", name: 'Dota 2, no noobs', size: 5, public: TRUE, game: 'Dota 2', language: 'Swedish', creator_id: 1)
-    Room.create(url: "ihtwuw85za", name: 'Double AK+ MM', size: 5, public: TRUE, game: 'CS:GO', language: 'English', creator_id: 1)
-    Room.create(url: "czrnmssxsn", name: 'T6 Rifts (Paragon >100)', size: 4, public: TRUE, game: 'Diablo 3', language: 'Russian', creator_id: 1)
-    Room.create(url: "k4kyubiir3", name: 'ADK games', size: 2, public: TRUE, game: 'Achtung Die Kurwe', language: 'Russian', creator_id: 1)
-    Room.create(url: "insj4ie8ul", name: 'Please join :(', size: 4, public: TRUE, game: 'Diablo 2', language: 'Russian', creator_id: 1)
-    Room.create(url: "jdwail32ld", name: 'HoN MM (3000+ MMR)', size: 5, public: FALSE, game: 'Heroes of Newerth', language: 'Icelandic', creator_id: 1)
-    Room.create(url: "ngui1dk2lo", name: 'No-scoping MW2', size: 8, public: TRUE, game: 'Call of Duty', language: 'English', creator_id: 1)
+    Room.create(url: "417kd4cyfs", name: 'Dota 2, no noobs', size: 5, public: TRUE, game: 'Dota 2', language: 'Swedish', creator_id: 8594038655935)
+    Room.create(url: "ihtwuw85za", name: 'Double AK+ MM', size: 5, public: TRUE, game: 'CS:GO', language: 'English', creator_id: 890128502182)
+    Room.create(url: "czrnmssxsn", name: 'T6 Rifts (Paragon >100)', size: 4, public: TRUE, game: 'Diablo 3', language: 'Russian', creator_id: 12895082019)
+    Room.create(url: "k4kyubiir3", name: 'ADK games', size: 2, public: TRUE, game: 'Achtung Die Kurwe', language: 'Russian', creator_id: 58325230142)
+    Room.create(url: "insj4ie8ul", name: 'Please join :(', size: 4, public: TRUE, game: 'Diablo 2', language: 'Russian', creator_id: 214891025801)
+    Room.create(url: "jdwail32ld", name: 'HoN MM (3000+ MMR)', size: 5, public: FALSE, game: 'Heroes of Newerth', language: 'Icelandic', creator_id: 59302589032)
+    Room.create(url: "ngui1dk2lo", name: 'Ludwig creator test', size: 8, public: TRUE, game: 'Call of Duty', language: 'English', creator_id: 117555853306123120083)
+    Room.create(url: "ngui1dk2lo", name: 'Lukas creator test', size: 8, public: TRUE, game: 'Call of Duty', language: 'English', creator_id: 109448850811394477033)
+    Room.create(url: "ngui1dk2lo", name: 'Albin creator test', size: 8, public: TRUE, game: 'Call of Duty', language: 'English', creator_id: 76561198033810799)
   end
 
   def self.user
