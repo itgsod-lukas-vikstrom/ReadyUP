9201f0a1767c2d1f16b57caa0ea057f3956f00fb
itgsod-lukas-vikstrom <lukas.vikstrom@elev.it-gymnasiet.se>
2015-05-12 10:21:20 +0200

    Finetuned web socket member refresh

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ $(function(){
     ws.onmessage = function(evt) {
         console.log(evt.data)
         if( evt.data.indexOf('Checked') >= 0){
-            console.log('REFRESH PLS')
+            console.log('Refreshed members')
             $("#Members").load(location.href + " #Members");
         }
         if ($('#chat tbody tr:last').length > 0){
@@ -13,7 +13,7 @@ $(function(){
             $('#chat tbody').append('<tr><td>' + evt.data + '</td></tr>');
         }
     };
-
+/*
     ws.onclose = function() {
 
     };
@@ -21,16 +21,13 @@ $(function(){
     ws.onopen = function() {
 
 
-    };
+    };*/
 
 
     $("form.form-stacked").submit(function(e) {
-
         if($("#msg").val().length > 0){
             ws.send($("#msg").val());
-
             $("#msg").val("");
-
         }
         return false;
     });
@@ -41,3 +38,12 @@ $(function(){
     });
 
 });
+/*
+window.addEventListener("beforeunload", function (e) {
+
+    var confirmationMessage = 'Leaving this page will uncheck you from the room';
+
+
+    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+});*/
